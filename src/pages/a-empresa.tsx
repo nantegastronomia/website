f8a8af1c62da45a11012e7d2677466ff5282d989
leonardorramos <leonardorramos@gmail.com>
2023-01-27 15:30:56 -0300

    add zap button to all pages

--- src/pages/a-empresa.tsx
+++ src/pages/a-empresa.tsx
@@ -1,54 +1,65 @@
-import type { HeadFC, PageProps } from "gatsby"
-import React from "react"
-import { H1 } from "ui/typo"
-import Layout from "../ui/components/layout"
+import type { HeadFC, PageProps } from "gatsby";
+import React from "react";
+import ZapButton from "ui/components/zapbutton/ZapButton";
+import { H1 } from "ui/typo";
+import Layout from "../ui/components/layout";
 
-import { Base, Container, SubTit, Titulo } from "../ui/containers"
+import { Base, Container, SubTit, Titulo } from "../ui/containers";
 
-export const Head: HeadFC = () => <title>Nantê Gastronomia</title>
+export const Head: HeadFC = () => <title>Nantê Gastronomia</title>;
 
 const IndexPage: React.FC<PageProps> = () => {
-	return (
-		<Layout>
-			<Base style={{ backgroundColor: "#FFF8EB", minHeight: "78.2vh" }}>
-				<Container>
-					<Titulo>
-						<H1 as={"h2"} color={"y2"}>
-							A Empresa
-						</H1>
-						<SubTit>
-							O Nantê nasceu 12 anos atrás, atendendo exclusivamente a uma
-							grande empresa do mercado de eventos sociais no Rio de Janeiro, o
-							que tomava 100% da nossa disponibilidade.
-						</SubTit>
-						<SubTit>
-							Crescemos, nos estruturamos ainda mais e, enfim, tomamos a decisão
-							de ir para o mercado formal, afinal, sempre fomos reconhecidos
-							pela qualidade da nossa entrega e não fazia sentido algum nos
-							limitarmos a um único cliente.
-						</SubTit>
-						<SubTit>Foi que o que fizemos!</SubTit>
-						<SubTit>
-							Levar comida boa, gostosa e com segurança para todo esse Brasil
-							tem sido um saboroso desafio, uma jornada intensa, mas muito
-							gratificante, pois alimentar pessoas é algo de fato encantador.
-						</SubTit>
-						<SubTit>
-							Convidamos você a vir junto com a gente fazer parte dessa história
-							de sucesso e voarmos voos ainda mais altos, levando tudo de melhor
-							por onde passar.
-						</SubTit>
-						<SubTit>Vamos nessa?!</SubTit>
-						<SubTit>
-							CONTATO: <br />
-							WhatsApp: 21 2391-4422 <br />
-							E-mail: sabor@nante.com.br
-						</SubTit>
-					</Titulo>
-				</Container>
-			</Base>
-		</Layout>
-	)
-}
+  return (
+    <Layout>
+      <Base style={{ backgroundColor: "#FFF8EB", minHeight: "78.2vh" }}>
+        <Container>
+          <Titulo>
+            <H1 as={"h2"} color={"y2"}>
+              A Empresa
+            </H1>
+            <SubTit>
+              O Nantê nasceu 12 anos atrás, atendendo exclusivamente a uma
+              grande empresa do mercado de eventos sociais no Rio de Janeiro, o
+              que tomava 100% da nossa disponibilidade.
+            </SubTit>
+            <SubTit>
+              Crescemos, nos estruturamos ainda mais e, enfim, tomamos a decisão
+              de ir para o mercado formal, afinal, sempre fomos reconhecidos
+              pela qualidade da nossa entrega e não fazia sentido algum nos
+              limitarmos a um único cliente.
+            </SubTit>
+            <SubTit>Foi que o que fizemos!</SubTit>
+            <SubTit>
+              Levar comida boa, gostosa e com segurança para todo esse Brasil
+              tem sido um saboroso desafio, uma jornada intensa, mas muito
+              gratificante, pois alimentar pessoas é algo de fato encantador.
+            </SubTit>
+            <SubTit>
+              Convidamos você a vir junto com a gente fazer parte dessa história
+              de sucesso e voarmos voos ainda mais altos, levando tudo de melhor
+              por onde passar.
+            </SubTit>
+            <SubTit>Vamos nessa?!</SubTit>
+            <SubTit>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  //   width: "100%",
+                }}
+              >
+                <ZapButton />
+                <p style={{ textAlign: "center" }}>
+                  E-mail: sabor@nante.com.br
+                </p>
+              </div>
+            </SubTit>
+          </Titulo>
+        </Container>
+      </Base>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
--- src/ui/containers.ts
+++ src/ui/containers.ts
@@ -1,69 +1,72 @@
-import { styled } from "stitches.config"
+import { styled } from "stitches.config";
 
 export const Box = styled("div", {
-	display: "flex",
-	gap: "$1",
-	justifyContent: "start",
+  display: "flex",
+  gap: "$1",
+  justifyContent: "start",
 
-	variants: {
-		gap: {
-			1: {
-				gap: "$1",
-			},
-			2: {
-				gap: "$2",
-			},
-			3: {
-				gap: "$3",
-			},
-			4: {
-				gap: "$4",
-			},
-			5: {
-				gap: "$5",
-			},
-		},
-		dir: {
-			col: { flexDirection: "column" },
-			row: { flexDirection: "row" },
-		},
+  variants: {
+    gap: {
+      1: {
+        gap: "$1",
+      },
+      2: {
+        gap: "$2",
+      },
+      3: {
+        gap: "$3",
+      },
+      4: {
+        gap: "$4",
+      },
+      5: {
+        gap: "$5",
+      },
+    },
+    dir: {
+      col: { flexDirection: "column" },
+      row: { flexDirection: "row" },
+    },
 
-		vAlingn: {
-			center: {
-				alignItems: "center",
-			},
-		},
+    vAlingn: {
+      center: {
+        alignItems: "center",
+      },
+    },
 
-		hAlingn: {
-			start: {
-				justifyContent: "start",
-			},
-			center: {
-				justifyContent: "center",
-			},
-		},
-	},
-})
+    hAlingn: {
+      start: {
+        justifyContent: "start",
+      },
+      center: {
+        justifyContent: "center",
+      },
+    },
+  },
+});
 
 export const ContainerCenter = styled("section", {
-	maxWidth: "$6",
-	margin: "0 auto",
-	display: "flex",
-	width: "100%",
-})
+  maxWidth: "$6",
+  margin: "0 auto",
+  display: "flex",
+  width: "100%",
+});
 
 export const Base = styled("div", {
-	paddingX: "$6",
-	paddingY: "$4",
-})
+  paddingX: "$6",
+  paddingY: "$4",
+});
 
 export const Container = styled(ContainerCenter, {
-	gap: "$8",
-	flexDirection: "column",
-})
+  gap: "$8",
+  flexDirection: "column",
+});
 
 export const Titulo = styled("div", {
-	width: "100%",
-})
+  width: "100%",
+});
 
-export const SubTit = styled(Titulo, { marginTop: "$2", lineHeight: "1.4rem" })
+export const SubTit = styled(Titulo, {
+  marginTop: "$2",
+  lineHeight: "1.4rem",
+});
